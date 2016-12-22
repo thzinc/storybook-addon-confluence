bfbbbb14d8544e4de893a304d70d03d58e65f409
Daniel James <djames@syncromatics.com>
2016-12-21 18:19:37 -0800

    Fixed same bug with module format

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var addons = require('@kadira/storybook-addons');
+var addons = require('@kadira/storybook-addons').default;
 var ADD_PAGE_EVENT = require('./constants').ADD_PAGE_EVENT;
 
 module.exports = function WithConfluencePage(props) {
